45829a9fe3e50ed1ef9f50c9dbf352be82e2ee46
Goran Tomasic <goran.tomasic@infobip.com>
2016-09-23 13:22:39 +0200

    Removed redundant alerts

--- MobileMessagingPlugin/www/MobileMessagingCordova.js
+++ MobileMessagingPlugin/www/MobileMessagingCordova.js
@@ -1,11 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.start = function(success, error) {
-    alert("Cordova sucks - start method exported correctly");
     exec(success, error, "MobileMessagingCordova", "start");
 };
 
 exports.showAlert = function(message, success, error) {
-    alert("Will show alert!");
     exec(success, error, "MobileMessagingCordova", "showAlert", [message]);
 }
