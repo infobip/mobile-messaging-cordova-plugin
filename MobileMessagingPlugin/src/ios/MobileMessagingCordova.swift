6fa20008a2b3959bcea7ebaec365f9216efbe024
Goran Tomasic <goran.tomasic@infobip.com>
2016-09-23 13:18:42 +0200

    Example code expanded

--- MobileMessagingPlugin/src/ios/MobileMessagingCordova.swift
+++ MobileMessagingPlugin/src/ios/MobileMessagingCordova.swift
@@ -1,13 +1,42 @@
 import Foundation
+import UIKit
+import MobileMessaging
 
 @objc(MobileMessagingCordova) class MobileMessagingCordova : CDVPlugin {
   func start(command: CDVInvokedUrlCommand) {
     var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
 
-    // do work here
+    // do work here 
 
     pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
 
+    self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId)
+  }
+
+  func showAlert(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+
+    // extract the message argument
+    let alertMsg = command.arguments[0] as? String ?? ""
+
+    if alertMsg.characters.count > 0 {
+      // create the AlertController
+      let alertController = UIAlertController(title: "Cordova Demo", message: alertMsg, preferredStyle: .Alert)
+
+      let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction) in
+        // whatevs
+      })
+
+      let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+
+      alertController.addAction(okAction)
+      alertController.addAction(cancelAction)
+
+      UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
+    }
+
+    // return OK to Cordova
+    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
     self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId)
   }
 }
--- MobileMessagingPlugin/www/MobileMessagingCordova.js
+++ MobileMessagingPlugin/www/MobileMessagingCordova.js
@@ -1,6 +1,11 @@
 var exec = require('cordova/exec');
 
 exports.start = function(success, error) {
-    alert("Cordova sucks - start method exported correctly")
+    alert("Cordova sucks - start method exported correctly");
     exec(success, error, "MobileMessagingCordova", "start");
 };
+
+exports.showAlert = function(message, success, error) {
+    alert("Will show alert!");
+    exec(success, error, "MobileMessagingCordova", "showAlert", [message]);
+}
