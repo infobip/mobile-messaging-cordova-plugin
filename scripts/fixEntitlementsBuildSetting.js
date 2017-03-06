d5477a9c0e635bd51a54bbb0a9d91d9ab1fce0bd
Andrey K <kaskaaddnb@gmail.com>
2017-03-06 13:09:11 +0300

    plugin improvements. entitlements setting - automation needed

--- scripts/fixEntitlementsBuildSetting.js
+++ scripts/fixEntitlementsBuildSetting.js
@@ -1,8 +1,12 @@
+// this code comments out the CODE_SIGN_ENTITLEMENTS setting in the build.xcconfig
+// in order to get rid of following build process warning:
+// warning: Falling back to contents of entitlements file "Entitlements-Debug.plist" because it was modified during the build process. Modifying the entitlements file during the build is unsupported.error: The file “Entitlements-Debug.plist” couldn’t be opened because there is no such file.
+// The appropriate CODE_SIGN_ENTITLEMENTS will be set for the project directly through the project build settings
 var fs = require('fs');
 var xcconfigFile = 'platforms/ios/cordova/build.xcconfig';
 var text = fs.readFileSync(xcconfigFile, 'utf-8');
 var idx = text.search(/^\s?CODE_SIGN_ENTITLEMENTS/gm);
 if (idx != -1) {
-    var newText = text.slice(0, idx) + "//" + text.slice(idx);
+    var newText = text.slice(0, idx) + "// [this line was commented out automatically by MobileMessagingCordova plugin hook due to a Cordova issue CB-12212] " + text.slice(idx);
     fs.writeFileSync(xcconfigFile, newText, 'utf-8');
 }
