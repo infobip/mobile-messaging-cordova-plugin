ceba57b84f347c39deaabf2461e09e46cdce93ab
Andrey K <kaskaaddnb@gmail.com>
2017-03-06 16:12:34 +0300

    review fixes

--- scripts/fixEntitlementsBuildSetting.js
+++ scripts/fixEntitlementsBuildSetting.js
@@ -1,6 +1,5 @@
 // this code comments out the CODE_SIGN_ENTITLEMENTS setting in the build.xcconfig
-// in order to get rid of following build process warning:
-// warning: Falling back to contents of entitlements file "Entitlements-Debug.plist" because it was modified during the build process. Modifying the entitlements file during the build is unsupported.error: The file “Entitlements-Debug.plist” couldn’t be opened because there is no such file.
+// in order to get rid of following build process warning: "Falling back to contents of entitlements file "Entitlements-Debug.plist" because it was modified during the build process. Modifying the entitlements file during the build is unsupported.error: The file “Entitlements-Debug.plist” couldn’t be opened because there is no such file.""
 var fs = require('fs');
 var xcconfigFile = 'platforms/ios/cordova/build.xcconfig';
 var text = fs.readFileSync(xcconfigFile, 'utf-8');
