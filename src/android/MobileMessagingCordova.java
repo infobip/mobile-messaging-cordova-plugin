c871ebdaa3cc15c0a7571cb9205c90c2af6cf6f3
okoroleva <olga.koroleva@infobip.com>
2017-07-17 17:32:05 +0300

    import org.infobip.mobile.messaging.MobileMessagingCore;

--- src/android/MobileMessagingCordova.java
+++ src/android/MobileMessagingCordova.java
@@ -25,6 +25,7 @@ import org.infobip.mobile.messaging.Event;
 import org.infobip.mobile.messaging.Message;
 import org.infobip.mobile.messaging.MobileMessaging;
 import org.infobip.mobile.messaging.MobileMessagingLogger;
+import org.infobip.mobile.messaging.MobileMessagingCore;
 import org.infobip.mobile.messaging.MobileMessagingProperty;
 import org.infobip.mobile.messaging.UserData;
 import org.infobip.mobile.messaging.api.support.http.serialization.JsonSerializer;
