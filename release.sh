ea507e6d91eb3168efa57fda296831abf81913dd
Stanislav Slavin <slavin.stas@gmail.com>
2018-04-23 13:16:00 +0300

    Added release script
    Former-commit-id: 7de395a468be23e93cc7637f12bea927f98ce8d1

---
+++ release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git remote rm github
+
+# Find last git tag and create commit log
+LAST_TAG=`git describe --tags --abbrev=0`
+RELEASE_COMMIT_LOG=`git log $LAST_TAG..HEAD --oneline`
+
+# Save commit log to property file as a property
+# (replacing newlines with "\n")
+echo RELEASE_COMMIT_LOG="${RELEASE_COMMIT_LOG//$'\n'/\\n}" > $PROPERTIES_FILE
+
+# Bump version
+./newversion $RELEASE_VERSION
+
+# Install Carthage
+# brew install carthage
+cd libs/ios/
+/usr/local/bin/carthage update --cache-builds
+rm -f Carthage/Build/iOS/*.bcsymbolmap
+rm -f Carthage/Build/iOS/*.dSYM
+cd ../../
+
+git add .
+
+# Commit release version
+git commit -a -m "Release: $RELEASE_VERSION"
+
+# Create and push tag
+git tag $RELEASE_VERSION -m "Release: $RELEASE_VERSION"
+
+# Push changes
+git push origin master --tags
