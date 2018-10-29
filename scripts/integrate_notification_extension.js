c5bf9c571c8c8a7c64adbb203236a6485af36bfd
Andrey K <kaskaaddnb@gmail.com>
2018-10-29 14:08:08 +0300

    don't run integrate notification extension if iOS platform is not set

--- scripts/integrate_notification_extension.js
+++ scripts/integrate_notification_extension.js
@@ -1,6 +1,12 @@
 module.exports = function(ctx) {
+	if (ctx.opts.platforms.indexOf('ios') < 0) { // project doesn't support ios at all
+        return;
+    }
+    if (ctx.opts.cordova.platforms.length > 0 && ctx.opts.cordova.platforms.indexOf('ios') < 0) { // corodova prepare was explicitly called for non-ios platforms
+        return;
+    }
     if (!(ctx.opts.options.APPLICATION_CODE && ctx.opts.options.PROJECT && ctx.opts.options.TARGET && ctx.opts.options.APP_GROUP)) {
-        return
+        return;
     }
     var command = ` sudo gem install mmine;
                     mmine integrate --cordova\
