7bddd697538b99f21100921ebc82754a0761ee74
Olga Koroleva <Olga.Koroleva@infobip.com>
2021-08-25 12:00:11 +0300

    override extension signing added

--- scripts/integrate_notification_extension.js
+++ scripts/integrate_notification_extension.js
@@ -18,10 +18,17 @@ module.exports = function(ctx) {
     var variables = pluginConfig.variables;
     var appName = appConfig.name();
 
+    if (ctx.opts.options === undefined) {
+        console.log("WARNING: iOS platform is not added, mobile messaging plugin can't proceed. Call 'cordova prepare ios' after ios platform will be added.");
+        return;
+    }
+
     var appCode = ctx.opts.options.IOS_EXTENSION_APP_CODE || variables.IOS_EXTENSION_APP_CODE;
     var appGroup = ctx.opts.options.IOS_EXTENSION_APP_GROUP || variables.IOS_EXTENSION_APP_GROUP;
     var projectPath = ctx.opts.options.IOS_EXTENSION_PROJECT_PATH || variables.IOS_EXTENSION_PROJECT_PATH || `platforms/ios/${appName}.xcodeproj`;
     var projectMainTarget = ctx.opts.options.IOS_EXTENSION_PROJECT_MAIN_TARGET || variables.IOS_EXTENSION_PROJECT_MAIN_TARGET || appName;
+    var overrideSigning = ctx.opts.options.IOS_OVERRIDE_EXTENSION_SIGNING || variables.IOS_OVERRIDE_EXTENSION_SIGNING;
+
 
     if (!(appCode && appGroup && projectPath && projectMainTarget)) {
         console.log("ERROR: 'IOS_EXTENSION_APP_CODE' or 'IOS_EXTENSION_APP_GROUP' or 'IOS_EXTENSION_PROJECT_PATH' or 'IOS_EXTENSION_PROJECT_MAIN_TARGET' not defined");
@@ -30,12 +37,22 @@ module.exports = function(ctx) {
     }
 
     var command = ` export GEM_HOME=plugins/${ctx.opts.plugin.id}/gems;
-                    gem install --install-dir plugins/${ctx.opts.plugin.id}/gems mmine -v 0.9.8;
+                    gem install --install-dir plugins/${ctx.opts.plugin.id}/gems mmine -v 0.9.9;
                     ./plugins/${ctx.opts.plugin.id}/gems/bin/mmine integrate -a ${appCode}\
                     -p "${ctx.opts.projectRoot}/${projectPath}"\
                     -t "${projectMainTarget}"\
-                    -g ${appGroup} -c -x
-                    export GEM_HOME=$GEM_PATH`;
+                    -g ${appGroup}\
+                    -c -x`;
+
+    if (overrideSigning === "true") {
+        command += '-s ';
+    }
+    command += ';';
+    command += '\nexport GEM_HOME=$GEM_PATH';
+
+    console.log("Command:  " + command);
+
+
 
     var exec = require('child_process').exec, child;
     child = exec(command,
